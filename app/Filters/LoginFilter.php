f856fe90c19e5cdf5ba7ca2a08e9505c86c1d746
Muhammad Azmi <muhammad4zmi@gmail.com>
2020-08-25 11:50:32 +0700

    Add files via upload

---
+++ app/Filters/LoginFilter.php
@@ -0,0 +1,53 @@
+<?php
+
+namespace App\Filters;
+
+use CodeIgniter\HTTP\RequestInterface;
+use CodeIgniter\HTTP\ResponseInterface;
+use CodeIgniter\Filters\FilterInterface;
+
+class LoginFilter implements FilterInterface
+{
+    public function before(RequestInterface $request)
+    {
+        $db = \Config\Database::connect();
+        $request = \Config\Services::request();
+        if (!session('email')) // saya hanya membuat sederhana saja. silahkan kembangkan di kemudian hari
+        {
+            return redirect()->to('/auth');
+        } else {
+
+            $role_id = session()->get('role_id');
+
+            $menu = $request->uri->getSegment(1);
+
+
+            $queryMenu = $db->query("SELECT * FROM user_menu WHERE menu= '$menu'");
+            $hasil = $queryMenu->getRowArray();
+            // dd($hasil);
+            // die;
+            //$queryMenu = $db->table('user_menu')->getWhere(['menu' => $menu])->getRowArray();
+            $menu_id = $hasil['id'];
+            // dd($menu_id);
+            // die;
+            $userAccess = $db->query("SELECT * FROM user_access_menu 
+                                              WHERE role_id = $role_id 
+                                              AND menu_id = $menu_id");
+            $access = $userAccess->getRowArray();
+            // dd($access);
+            // die;
+
+            if ($access == null) {
+                return redirect()->to('/auth/blocked');
+            }
+        }
+        // Do something here
+    }
+
+    //--------------------------------------------------------------------
+
+    public function after(RequestInterface $request, ResponseInterface $response)
+    {
+        // Do something here
+    }
+}
